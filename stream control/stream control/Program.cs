94597b7c7265c433d614f7e58becd06bf4f4b668
asdbeen <asdbeen@foxmail.com>
2022-01-08 22:06:02 +0800

    20220108

---
+++ stream control/stream control/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*
+             * 流程控制关键词
+             * break 终止
+             * continue 继续
+             * return
+             * 
+             */
+
+            //int num = 2;
+            //switch(num)
+            //{
+            //    case 1:
+            //        Console.WriteLine("case 1");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("case 2");
+            //        break ;
+            //}
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine($"the first even number is {i}");
+            //        break;
+            //    }
+            //}
+
+            //while (true)
+            //{
+            //    break;
+            //}
+            //int sum = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum = sum + i;
+            //        continue; //back to i++
+            //    }
+            //}
+            //Console.WriteLine($"sum is {sum}");
+            
+            //// code cant be below continue 
+        }
+
+
+        //private static void SayHi(string name)
+        //{
+        //    if(name =="zhangsan")
+        //    {
+        //        Console.WriteLine($"hi, {name}");
+        //        return;
+        //    }
+
+        //    if (name == "lisi")
+        //    {
+        //        Console.WriteLine($"fuckoff, {name}");
+        //    }
+        //}
+
+
+        private static string extractor(string fruit)
+        {
+            string result = fruit + "juice";
+
+            return result;
+        }
+
+
+    }
+}
---
+++ string/string learning/Program.cs
@@ -0,0 +1,55 @@
+﻿string email = "286507689@qq.com";
+//int index = email.IndexOf("@");    // it will only find it from left to right, and only one string
+//// int index = email.LastIndexOf("@")
+//Console.WriteLine("@ index is ： "+ index);
+
+//if (index < 0)
+//    {
+//    Console.WriteLine("format is wrong");
+//    }
+//else
+//    {
+//    Console.WriteLine("format is correct");
+//    }
+
+//string newStr = email.Substring(10);
+//Console.WriteLine(newStr);
+
+//string newStr = email.Substring(email.IndexOf("@")+1,2);  // limit the number of output
+//Console.WriteLine(newStr);
+
+//Console.WriteLine(email.ToUpper());
+
+
+//"==" is used for address,  equals is for value
+
+//if (email == "286507689@qq.com")
+//{
+//    Console.WriteLine("==");
+//}
+
+//string.Join("-",strs);
+
+
+string[] strs = { "a ".Trim(), "b", "c" };  //delete space
+string newStr = string.Join("-",strs);
+string[] newStr2 = newStr.Split("-");
+for (int i = 0; i < newStr2.Length; i++)
+{
+    Console.WriteLine(newStr2[i]);
+}
+
+string str2 = Console.ReadLine();
+
+if (str2.Equals("asd"))
+{
+    Console.WriteLine("correct");
+}
+
+else
+{
+    Console.WriteLine("wrong");
+}
+
+Console.Read();
+
