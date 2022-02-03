c3068a6344ed664a3d611fadd1e31f264715c02a
asdbeen <asdbeen@foxmail.com>
2022-02-04 00:27:11 +0800

    20220204

---
+++ NotifyChangeEvent/NotifyChangeEvent/Model/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace NotifyChangeEvent.Model
+{
+    class Employee : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get 
+            { 
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        private string _fullName;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return "Mr" + _name;
+            }
+
+            set
+            {
+                _fullName = value;
+            }
+        }
+    }
+}
