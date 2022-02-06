1a04143e7a90aba542f077f3cba1f3af5e85af66
asdbeen <asdbeen@foxmail.com>
2022-02-06 20:01:49 +0800

    20220206

---
+++ Relative Source/RelativeSource/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RelativeSource
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Item> LstItem = new List<Item>();
+
+            LstItem.Add(new Item() { Data = 100 });
+            LstItem.Add(new Item() { Data = 130 });
+            LstItem.Add(new Item() { Data = 150 });
+
+            DataContext = LstItem;
+        }
+    }
+
+    public class Item
+    {
+        public int Data { get; set; }
+    }
+}
