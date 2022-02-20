f641144d28b961b0515f12fb4d57cad464a70858
asdbeen <asdbeen@foxmail.com>
2022-02-21 00:38:02 +0800

    20220221

---
+++ Asynchrone/Asynchrone Programmierung/Catalog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchrone_Programming
+{
+    public class Catalog
+    {
+        public class Coffee { }
+
+        public class Egg { }
+
+        public class Bacon { }
+
+        public class Toast { }
+
+        public class Juice { }
+    }
+}
